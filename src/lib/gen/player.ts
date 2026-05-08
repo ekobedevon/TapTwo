139fb125d9a2a80541175a8d8bb78e2c69cdb1b4
Kobe Edmond <e.kobedevon@gmail.com>
2026-05-07 19:40:55 -0700

    player gen

--- src/lib/gen/player.ts
+++ src/lib/gen/player.ts
@@ -1,25 +1,54 @@
 import * as argon2 from 'argon2';
-import type { Insertable } from "kysely";
-import type { AuthUser } from "lib/db";
-import { nanoid } from "nanoid";
-import { db } from 'db';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import { PostgresDialect, Kysely, type SelectType, type Insertable } from 'kysely';
+import pg from 'pg';
+import type { AuthUser, DB } from 'lib/db';
+import { nanoid } from 'nanoid';
 
+// Load env file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const envPath = path.resolve(__dirname, '../../../.env');
+dotenv.config({ path: envPath, override: true });
 
-const hashed_password = await argon2.hash("password")
-let newUsers: Insertable<AuthUser>[] = []
+// Create DB Connection
+const dialect = new PostgresDialect({
+	pool: new pg.Pool({
+		database: process.env.DB_NAME,
+		host: process.env.PG_URL,
+		user: process.env.AUTH_PG_USER,
+		password: process.env.AUTH_PG_PASSWORD,
+		port: 5432,
+		max: 10
+	})
+});
+
+export const gen_db = new Kysely<DB>({
+	dialect
+});
+
+console.log('making new users');
+let newUsers: Insertable<AuthUser>[] = [];
 
 for (let index = 0; index < 10; index++) {
-    const id = nanoid(10)
-    const newUser:Insertable<AuthUser> = {
-        email: `${id}@taptwo.com`,
-        email_verified: true,
-        hashed_password,
-        id,
-        username:`user_${id}`
-    }
-    newUsers.push(newUser)
-    
+	const hashed_password = await argon2.hash('password');
+	const id = nanoid(10);
+	const newUser: Insertable<AuthUser> = {
+		email: `${id}@taptwo.com`,
+		email_verified: true,
+		hashed_password,
+		id,
+		username: `user_${id}`
+	};
+	newUsers.push(newUser);
 }
 
-const user_ids = await db.insertInto("auth_user").values(newUsers).returning("id").execute()
+const user_ids = await gen_db.insertInto('auth_user').values(newUsers).returning('id').execute();
+
+console.log(user_ids);
+
+await gen_db.destroy();
+process.exit(0);
