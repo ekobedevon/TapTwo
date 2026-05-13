29b4efe5dd5448ec65c2fce710449e6100c12f9d
Kobe Edmond <e.kobedevon@gmail.com>
2026-05-13 11:03:23 -0700

    DRAW

--- src/lib/db.d.ts
+++ src/lib/db.d.ts
@@ -11,7 +11,7 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
 
-export type MatchStatus = "LOSE" | "TIE" | "WIN";
+export type MatchStatus = "DRAW" | "LOSE" | "WIN";
 
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
